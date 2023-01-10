7a4545858938f72ea22bf0682904dfe0aa755720
Данил Сафин <fiackopurple2018@gmail.com>
2023-01-10 19:38:19 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function getRandomMessage(array) {
+    let ind = getRandomInt(0, array.length);
+    return array[ind];
+}
+
+let future = [
+    `Добро пожаловать в Гриффиндор!`,
+    `Тебя ждёт большая удача`,
+    `На обед будет что-то не вкусное`,
+    `Одевайся теплей`,
+    `Тебя ждёт финансовое благополучие`,
+    `Отлично выспишься сегодня!`,
+    `Домашку всё равно придётся сделать`,
+    `Кто-то сделает тебе комплимент`,
+    `Выходные пройдут отлично`,
+    `Шляпа устала, предсказания не будет`
+];
+
 let hatText = document.querySelector(".hat h1");
 let hatImg = document.querySelector(".hat img");
 
 hatImg.addEventListener("click", function() {
-    hatText.textContent = `Талон #${getRandomInt(1, 11)}`;
+    hatText.textContent = `${getRandomMessage(future)}`;
 });
