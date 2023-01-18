d0bc8e5447caf9cafc9245d7320e3e977c3d8f52
Данил Сафин <fiackopurple2018@gmail.com>
2023-01-18 19:36:26 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -48,20 +48,34 @@ function refresh_template() {
       error = true;
   }
   year = String(date.getFullYear());
-  time = String(date.getHours()) + ":" + String(date.getMinutes());
+  hours = String(date.getHours());
+  minutes = String(date.getMinutes());
+  if (hours.length == 1)
+    hours = `0${hours}`;
+  if (minutes.length == 1)
+    minutes = `0${minutes}`;
+  time = hours + ":" + minutes;
 
   delete date;
 
-  date = `<div class="card-footer text-muted">${num} ${month} ${year} ${time}</div>`;
+  date = `<div class="card-footer text-muted">${num} ${month} ${year} ${hours}:${minutes}</div>`;
 
-  if (date.indexOf("NaN") != -1)
+  if (date.indexOf("NaN") != -1 || textInput.value == ``)
     error = true;
 
   if (error) {
     alert("Ошибка");
     template = ``;
   }
+
   else {
+
+    if (categoryInput.value == ``)
+    categoryInput.value = 'Без категории';
+
+    if (titleInput.value == ``)
+        titleInput.value = 'Без названия'
+
     template = 
     `<div class="card">
       <div class="card-body">
@@ -82,8 +96,13 @@ let categoryInput = document.querySelector(`#category`);
 let dateInput = document.querySelector(`#date`);
 let notesNode = document.querySelector(`#notes`);
 let wordCount = document.querySelector(`#word-count`)
+let searchInput = document.querySelector(`#search`);
+let searchButton = document.querySelector(`#search-start`)
+let searchCancelButton = document.querySelector(`#search-cancel`);
 
 let notes = document.querySelectorAll(`.card`);
+
+wordCount.textContent = `0/140`;
 
 
 saveButton.addEventListener("click", function() {
@@ -100,14 +119,19 @@ saveButton.addEventListener("click", function() {
         notes[notes.length-1].classList.add("important");
       default:
         notes[notes.length-1].classList.add("card");
+    notesNode.innerHTML = ``;
+    for (let note of notes)
+        notesNode.innerHTML += note.outerHTML;
+
+    searchInput.value = ``;
+    textInput.value = ``;
+    titleInput.value = ``;
+    categoryInput.value = ``;
+    dateInput.value = ``;
+    template = ``;
+    wordCount.textContent = `0/140`;
     }
   }
-
-  textInput.value = ``;
-  titleInput.value = ``;
-  categoryInput.value = ``;
-  dateInput.value = ``;
-  template = ``;
 });
 
 
@@ -122,7 +146,64 @@ titleInput.addEventListener('input', function() {
         titleInput.value = titleInput.value.slice(0,40);
 });
 
-сategoryInput.addEventListener('input', function() {
-    if (сategoryInput.value.length > 40)
-    сategoryInput.value = сategoryInput.value.slice(0,40);
+categoryInput.addEventListener('input', function() {
+    if (categoryInput.value.length > 40)
+        categoryInput.value = categoryInput.value.slice(0,40);
+});
+
+
+searchButton.addEventListener('click', function() {
+    searchedNotes = [];
+    if (searchInput.value.indexOf(' ') == -1) {
+        for (let note of notes) {
+            indStart = -1;
+            foundType = -1;
+            if (note.querySelector(`.card-text`).textContent.indexOf(`#${searchInput.value}`) != -1) {
+                indStart = note.querySelector(`.card-text`).textContent.indexOf(`#${searchInput.value}`);
+                foundType = `text`;
+            }
+            else if (note.querySelector(`.card-title`).textContent.indexOf(`#${searchInput.value}`) != -1) {
+                indStart = note.querySelector(`.card-title`).textContent.indexOf(`#${searchInput.value}`);
+                foundType = `title`;
+            }
+            else if (note.querySelector(`.card-subtitle`).textContent.indexOf(`#${searchInput.value}`) != -1) {
+                indStart = note.querySelector(`.card-subtitle`).textContent.indexOf(`#${searchInput.value}`);
+                foundType = `category`;
+            }
+            text = ``;
+            switch (foundType) {
+                case 'text':
+                    text = note.querySelector(`.card-text`).textContent.slice(indStart);
+                    break;
+                case 'title':
+                    text = note.querySelector(`.card-title`).textContent.slice(indStart);
+                    break;
+                case 'category':
+                    text = note.querySelector(`.card-subtitle`).textContent.slice(indStart);
+                    break;
+            }
+
+            if (text === `#${searchInput.value}` || text.indexOf(' ') == searchInput.value.length + 1)
+                searchedNotes.push(note.cloneNode(true));
+        }
+        notesNode.innerHTML = ``;
+        for (let found of searchedNotes) {
+            for (let type of [`.card-text`, `.card-title`, `.card-subtitle`]) {
+                if (found.querySelector(type).textContent.indexOf(`#${searchInput.value}`) != -1)
+                    found.querySelector(type).innerHTML = found.querySelector(type).innerHTML.slice(0, found.querySelector(type).innerHTML.indexOf(`#${searchInput.value}`)) + `<span class='yellow'>#${searchInput.value}</span>` + found.querySelector(type).innerHTML.slice(found.querySelector(type).innerHTML.indexOf(`#${searchInput.value}`) + searchInput.value.length + 1)
+            }
+            notesNode.innerHTML += found.outerHTML;
+        }
+    }
+    else {
+        searchInput.value = ``;
+        alert('Ошибка');
+    }
+});
+
+searchCancelButton.addEventListener('click', function() {
+    notesNode.innerHTML = ``;
+    for (let note of notes)
+        notesNode.innerHTML += note.outerHTML;
+    searchInput.value = ``;
 });
