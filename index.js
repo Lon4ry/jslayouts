0ee94818f6bd89feeb50d010dba12b24ac272d10
Данил Сафин <fiackopurple2018@gmail.com>
2023-01-19 12:20:07 +0300

    modified:   index.css 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // весь шпион целиком
-let spy = document.getElementById(`spy`);
+let spy = document.querySelector(`#spy`);
 // сюда выводи координаты мышки
-let resultClick = document.getElementById(`result-click`);
+let resultClick = document.querySelector(`#result-click`);
+let resultType = document.querySelector(`#result-type`);
 
+document.addEventListener("mousemove", function (evt) {
+    resultClick.textContent = `${evt.clientX}px : ${evt.clientY}px`
+});
+
+document.addEventListener("click", function (evt) {
+    spy.style.left = `${evt.clientX-Number(getComputedStyle(spy).width.slice(0,getComputedStyle(spy).width.indexOf('px')))*2/3}px`;
+    spy.style.top = `${evt.clientY-Number(getComputedStyle(spy).height.slice(0,getComputedStyle(spy).height.indexOf('px')))*2/3}px`;
+});
+
+document.addEventListener('keydown', function (evt) {
+    resultType.textContent = evt.key;
+}); 
+
+document.addEventListener('keyup', function() {
+    resultType.textContent = '';
+});
