2723f73431fb5de22d56e9b8f92ce87f99cf603b
Данил Сафин <fiackopurple2018@gmail.com>
2023-02-27 21:18:30 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,49 +1,79 @@
-let pokemon = {
-    name: 'Пикачу',
-    filename: 'pikachu.png',
-    description: 'Спокойный и добрый. Использует электричество, удары хвоста и укусы острых зубов',
-    container: document.querySelector('.pokemon-container')
-};
+let pokemons = [
+    {
+      name: "Пикачу",
+      img: "pikachu",
+      description: `Спокойный и добрый. Использует электричество, удары хвоста и укусы острых зубов.`
+    },
+    {
+      name: "Бульбазавр",
+      img: "bullbasaur",
+      description: `Самый спокойный и миролюбивый покемон, может быть лидером и заботиться о других.`
+    },
+    {
+      name: "Джигглипафф",
+      img: "jigglypuff",
+      description: `Очаровательное существо, но если его обидеть, то надувает щёки, злится и даёт сдачи. Очень злопамятный!`
+    },
+    {
+      name: "Мяут",
+      img: "meowth",
+      description: `Задиристый и наглый покемон. Гуляет по ночам и ворует блестящие предметы и монетки.`
+    },
+    {
+      img: "psyduck",
+      name: "Псайдак",
+      description: `Добрый и наивный, прожорливый, глуповатый. После сильной головной боли создаёт мощную психическую атаку.`
+    },
+    {
+      img: "eevee",
+      name: "Иви",
+      description: `Жизнерадостный и игривый покемон, отличный выбор для начинающего тренера.`
+    }
+];
+for (let i = 0; i < pokemons.length; i++) {
+    pokemons[i].render = function() {
+        let node = document.createElement('div');
+        let imageContainer = document.createElement('div');
+        let descriptionContainer = document.createElement('div');
 
-pokemon.render = function() {
-    let node = document.createElement('div');
-    let imageContainer = document.createElement('div');
-    let descriptionContainer = document.createElement('div');
+        node.classList.add('card');
+        imageContainer.classList.add('images');
+        descriptionContainer.classList.add('card-body');
 
-    node.classList.add('card');
-    imageContainer.classList.add('images');
-    descriptionContainer.classList.add('card-body');
+        let heartImage = document.createElement('img');
+        let avatarImage = document.createElement('img');
+        
+        heartImage.classList.add('heart');
+        heartImage.src = `./assets/heart-fill.svg`;
+        avatarImage.classList.add('avatar');
+        avatarImage.src = `./assets/${this.img}.png`;
 
-    let heartImage = document.createElement('img');
-    let avatarImage = document.createElement('img');
-    
-    heartImage.classList.add('heart');
-    heartImage.src = `./assets/heart-fill.svg`;
-    avatarImage.classList.add('avatar');
-    avatarImage.src = `./assets/${this.filename}`;
+        imageContainer.append(heartImage);
+        imageContainer.append(avatarImage);
+        node.append(imageContainer);
+        delete imageContainer, heartImage, avatarImage;
 
-    imageContainer.append(heartImage);
-    imageContainer.append(avatarImage);
-    node.append(imageContainer);
-    delete imageContainer, heartImage, avatarImage;
+        console.log(imageContainer);
 
-    console.log(imageContainer);
+        let descriptionTitle = document.createElement('h5');
+        let description = document.createElement('p');
 
-    let descriptionTitle = document.createElement('h5');
-    let description = document.createElement('p');
+        descriptionTitle.classList.add('card-title');
+        descriptionTitle.textContent = this.name;
+        description.classList.add('card-text');
+        description.textContent = this.description;
 
-    descriptionTitle.classList.add('card-title');
-    descriptionTitle.textContent = this.name;
-    description.classList.add('card-text');
-    description.textContent = this.description;
+        descriptionContainer.append(descriptionTitle);
+        descriptionContainer.append(description);
+        node.append(descriptionContainer);
+        delete descriptionContainer, descriptionTitle, description;
 
-    descriptionContainer.append(descriptionTitle);
-    descriptionContainer.append(description);
-    node.append(descriptionContainer);
-    delete descriptionContainer, descriptionTitle, description;
+        node.addEventListener('click', function () {
+            this.classList.toggle('card_like');
+        });
 
-    this.container.append(node);
-    delete node;
-};
-
-pokemon.render();
+        document.querySelector('.pokemon-container').append(node);
+        delete node;
+    };
+    pokemons[i].render();
+}
