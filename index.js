36da3cf7ef121983f846be9c7bd902520d35938f
Данил Сафин <fiackopurple2018@gmail.com>
2023-02-27 20:24:35 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 let container = document.querySelector(`.container`);
 
 function renderAlien() {
-  let newAlien = document.createElement('img')
-  newAlien.src = './assets/alien.png';
-  newAlien.classList.add(`alien`);
-  newAlien.style.width = `20vw`;
-  newAlien.style.left = `${getRandomInt(0, 80)}vw`;
-  newAlien.style.top = `${getRandomInt(0, 50)}vh`;
-  newAlien.addEventListener('click', function() {
-    this.remove();
-  });
-  container.append(newAlien);
+  for (let i = 0; i < getRandomInt(5, 30); i++) {
+    let newAlien = document.createElement('img')
+    newAlien.src = './assets/alien.png';
+    newAlien.classList.add(`alien`);
+    newAlien.style.width = `${getRandomInt(3, 20)}vw`;
+    newAlien.style.left = `${getRandomInt(0, 80)}vw`;
+    newAlien.style.top = `${getRandomInt(0, 50)}vh`;
+    newAlien.addEventListener('click', function() {
+      this.remove();
+      document.querySelector('.result').textContent = document.querySelectorAll('img').length
+    });
+    container.append(newAlien);
+  }
+  document.querySelector('.result').textContent = document.querySelectorAll('img').length
 }
 
 renderAlien();
+
+setTimeout(timer, getRandomInt(5000, 10000));
+
+function timer() {
+  renderAlien();
+  setTimeout(timer, getRandomInt(5000, 10000))
+}
+
 
 
 // Создаёт случайные целые числа
